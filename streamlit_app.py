732692489a23fb72ec45c9bcf2bcea0c0ad78890
VishwasKasote <vishwas.kasote@onepointltd.com>
2025-07-12 23:02:59 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ st.write(
 cxn=st.connection("snowflake")
 session = cxn.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
 covert_tojson=smoothiefroot_response.json()
@@ -26,7 +26,7 @@ name_list = [item["name"] for item in covert_tojson if "name" in item]
 name_on_order=st.text_input("Name on Smoothie")
 ingirdent_list=st.multiselect('Choose upto 5 ingredents', name_list, max_selections=5)
 
-#ssf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ssf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
@@ -40,11 +40,11 @@ if ingirdent_list:
     for fruit_chosen in  ingirdent_list: 
       ingredients_string+= fruit_chosen+' '
       
-      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       
-      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       st.subheader(fruit_chosen+'Nutrition Information')
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
       ssf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       
       my_insert_stmt = """ insert into smoothies.public.orders(Ingredients,NAME_ON_ORDER)
