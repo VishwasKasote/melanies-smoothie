38b94ffb497a331c50c284ba7ce8a480800d6644
VishwasKasote <vishwas.kasote@onepointltd.com>
2025-07-12 18:38:07 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingirdent_list:
       my_insert_stmt = """ insert into smoothies.public.orders(Ingredients,NAME_ON_ORDER)
                 values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
       #st.write(my_insert_stmt)
-      time_to_insert=st.button('Submit Order')
+    time_to_insert=st.button('Submit Order')
     if time_to_insert:
       session.sql(my_insert_stmt).collect()
       st.success('Hi,'+name_on_order+',Your Smoothie is ordered!', icon="✅")
