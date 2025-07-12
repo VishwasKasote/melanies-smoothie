8fb555492dc9fe1de35611c5831306c7a6dfbb20
VishwasKasote <vishwas.kasote@onepointltd.com>
2025-07-12 18:36:56 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,30 +21,20 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/al
 covert_tojson=smoothiefroot_response.json()
 name_list = [item["name"] for item in covert_tojson if "name" in item]
 name_on_order=st.text_input("Name on Smoothie")
-
 ingirdent_list=st.multiselect('Choose upto 5 ingredents', name_list, max_selections=5)
 ssf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingirdent_list:
     #st.text(ingirdent_list)
-
     ingredients_string=''
-
     for fruit_chosen in  ingirdent_list: 
       ingredients_string+= fruit_chosen+' '
       st.subheader(fruit_chosen+'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
       ssf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    
-
       my_insert_stmt = """ insert into smoothies.public.orders(Ingredients,NAME_ON_ORDER)
                 values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-      st.write(my_insert_stmt)
-        
-      time_to_insert=st.button('Submit Order')
-     
-    
       #st.write(my_insert_stmt)
+      time_to_insert=st.button('Submit Order')
     if time_to_insert:
       session.sql(my_insert_stmt).collect()
       st.success('Hi,'+name_on_order+',Your Smoothie is ordered!', icon="✅")
