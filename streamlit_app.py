1eef097b2e4c7e04eddf46751633224688df723b
VishwasKasote <vishwas.kasote@onepointltd.com>
2025-07-12 16:57:25 +0330

    Update streamlit_app.py
    import request get the new url

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+
+
+smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/orange")
+st.text(smoothiefroot_response)
+
 
 # Write directly to the app
 st.title(f"Customize your smoothie:cup_with_straw:")
