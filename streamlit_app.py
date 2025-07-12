fd9ae8abdcdb12487814338c73e2fe80e85c3840
VishwasKasote <vishwas.kasote@onepointltd.com>
2025-07-12 17:42:40 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cxn.session()
 #my_dataframe = session.table("smoothies.public.fruit_options")
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/all")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
 st.text(smoothiefroot_response.json())
 
 name_on_order=st.text_input("Name on Smoothie")
